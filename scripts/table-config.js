cfdc67d21010d4990d88b262e3b8b0dc2c545078
Jiseok CHOI <jiseok.dev@gmail.com>
2023-03-27 15:25:14 +0900

    feat: markdown table

---
+++ scripts/table-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  border: {
+    topBody: "",
+    topJoin: "",
+    topLeft: "",
+    topRight: "",
+    bottomBody: "",
+    bottomJoin: "",
+    bottomLeft: "",
+    bottomRight: "",
+    bodyLeft: "|",
+    bodyRight: "|",
+    bodyJoin: "|",
+    joinBody: "-",
+    joinLeft: "|",
+    joinRight: "|",
+    joinJoin: "|",
+  },
+  drawHorizontalLine: (index) => index === 1,
+};
